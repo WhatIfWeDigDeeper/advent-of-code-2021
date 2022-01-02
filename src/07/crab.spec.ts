8fe62cc197963c7ce3491ce965ce989904766037
What If We Dig Deeper <1247548+WhatIfWeDigDeeper@users.noreply.github.com>
2022-01-02 15:13:18 -0600

    part 2 (#5)

--- src/07/crab.spec.ts
+++ src/07/crab.spec.ts
@@ -1,11 +1,28 @@
-import { calculateEfficientAlignmentFuelCost } from './crab';
+import {
+  calculateEfficientAlignmentProgressiveFuelCost,
+  calculateEfficientAlignmentStandardFuelCost,
+} from './crab';
 
-test('sample', () => {
-  const actual = calculateEfficientAlignmentFuelCost('sample.txt');
-  expect(actual).toEqual(37);
+describe('part 1 standard fuel cost', (): void => {
+  test('sample', () => {
+    const actual = calculateEfficientAlignmentStandardFuelCost('sample.txt');
+    expect(actual).toEqual(37);
+  });
+
+  test('input', () => {
+    const actual = calculateEfficientAlignmentStandardFuelCost('input.txt');
+    expect(actual).toEqual(336721);
+  });
 });
 
-test('input', () => {
-  const actual = calculateEfficientAlignmentFuelCost('input.txt');
-  expect(actual).toEqual(336721);
+describe('part 2 progressive fuel cost', (): void => {
+  test('sample', () => {
+    const actual = calculateEfficientAlignmentProgressiveFuelCost('sample.txt');
+    expect(actual).toEqual(168);
+  });
+
+  test('input', () => {
+    const actual = calculateEfficientAlignmentProgressiveFuelCost('input.txt');
+    expect(actual).toEqual(91638945);
+  });
 });
--- src/07/crab.ts
+++ src/07/crab.ts
@@ -1,15 +1,69 @@
 import { readAllLines } from '../fileUtils';
 
-const calculateFuelCost = (positions: number[], alignment: number): number =>
+type CalculateFuelCost = (positions: number[], alignment: number) => number;
+
+const calculateStandardFuelCost = (
+  positions: number[],
+  alignment: number
+): number =>
   positions.reduce((acc, position) => acc + Math.abs(position - alignment), 0);
 
-export function calculateEfficientAlignmentFuelCost(fileName: string): number {
+function sumFuel(steps: number): number {
+  let fuelTotal = 0;
+  for (let i = 1; i <= steps; i++) {
+    fuelTotal += i;
+  }
+  return fuelTotal;
+  // this was too inefficient
+  // return steps === 0
+  //   ? 0
+  //   : createRange(steps, 1).reduce((acc, step) => acc + step, 0);
+}
+
+export const calculateProgressiveFuelCost = (
+  positions: number[],
+  alignment: number
+): number =>
+  positions.reduce(
+    (acc, position) => acc + sumFuel(Math.abs(position - alignment)),
+    0
+  );
+
+function range(size: number, startAt = 0): ReadonlyArray<number> {
+  return [...Array(size).keys()].map((i) => i + startAt);
+}
+
+export function calculateEfficientAlignmentFuelCost(
+  fileName: string,
+  calculateFuelCost: CalculateFuelCost
+): number {
   const positions: number[] = readAllLines(`07/${fileName}`)[0]
     .split(',')
     .map(Number)
     .sort((a, b) => a - b);
-  return positions.reduce((acc, position) => {
-    const fuelCost = calculateFuelCost(positions, position);
+  const min = positions[0];
+  const max = positions[positions.length - 1];
+
+  return range(max - min + 1, min).reduce((acc, rangeIndex) => {
+    const fuelCost = calculateFuelCost(positions, rangeIndex);
     return fuelCost < acc ? fuelCost : acc;
   }, Number.MAX_SAFE_INTEGER);
+}
+
+export function calculateEfficientAlignmentStandardFuelCost(
+  fileName: string
+): number {
+  return calculateEfficientAlignmentFuelCost(
+    fileName,
+    calculateStandardFuelCost
+  );
+}
+
+export function calculateEfficientAlignmentProgressiveFuelCost(
+  fileName: string
+): number {
+  return calculateEfficientAlignmentFuelCost(
+    fileName,
+    calculateProgressiveFuelCost
+  );
 }
